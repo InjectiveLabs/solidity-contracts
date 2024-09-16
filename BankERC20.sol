284e025c5223f1c967235562c18a9e292f7407e6
kakysha <kakysha@me.com>
2024-09-17 01:17:29 +0300

    make BankERC20 abstract
    + FixedSupplyBankERC20

--- BankERC20.sol
+++ BankERC20.sol
@@ -4,15 +4,11 @@ pragma solidity ^0.8.0;
 import {IBankModule} from "./Bank.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract BankERC20 is ERC20 {
+abstract contract BankERC20 is ERC20 {
     address constant bankContract = 0x0000000000000000000000000000000000000064;
     IBankModule bank = IBankModule(bankContract);
 
-     uint constant _initial_supply = 100 * (10**18); // CHANGE THIS TO YOUR DESIRED INITIAL SUPPLY
-
-
     constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20("","") { // parent ERC20 metadata is not used
-        _mint(msg.sender, _initial_supply);
         bank.setMetadata(name_, symbol_, decimals_);
     }
 
