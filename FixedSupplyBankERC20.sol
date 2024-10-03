55582d38d70e1be4094ebaedd8937c3e994cf0f9
kakysha <kakysha@me.com>
2024-10-03 19:27:09 +0300

    FixedSupplyERC20: initial supply as constructor argument

--- FixedSupplyBankERC20.sol
+++ FixedSupplyBankERC20.sol
@@ -4,10 +4,9 @@ pragma solidity ^0.8.0;
 import {BankERC20} from "./BankERC20.sol";
 
 contract FixedSupplyBankERC20 is BankERC20 {
-     
-     uint constant _initial_supply = 100 * (10**18); // CHANGE THIS TO YOUR DESIRED INITIAL SUPPLY
-
-    constructor(string memory name_, string memory symbol_, uint8 decimals_) BankERC20(name_, symbol_, decimals_) {
-        _mint(msg.sender, _initial_supply);
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint initial_supply_) BankERC20(name_, symbol_, decimals_) {
+        if (initial_supply_ > 0) {
+            _mint(msg.sender, initial_supply_);
+        }
     }
 }
