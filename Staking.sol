2e5e1b140a66e5222098b65865904f740d973122
Martin Arrivets <arrivets@gmail.com>
2025-01-20 17:33:38 +0100

    feat: add Staking.sol

---
+++ Staking.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface IStakingModule {
+    /// @dev Defines a method for performing a delegation of coins from a delegator to a validator.
+    /// @param delegatorAddress The address of the delegator
+    /// @param validatorAddress The address of the validator
+    /// @param amount The amount of the bond denomination to be delegated to the validator.
+    /// This amount should use the bond denomination precision stored in the bank metadata.
+    /// @return success Whether or not the delegate was successful
+    function delegate(
+        address delegatorAddress,
+        string memory validatorAddress,
+        uint256 amount
+    ) external returns (bool success);
+
+    /// @dev Defines a method for performing an undelegation from a delegate and a validator.
+    /// @param delegatorAddress The address of the delegator
+    /// @param validatorAddress The address of the validator
+    /// @param amount The amount of the bond denomination to be undelegated from the validator.
+    /// This amount should use the bond denomination precision stored in the bank metadata.
+    /// @return success Whether or not the undelegate was successful
+    function undelegate(
+        address delegatorAddress,
+        string memory validatorAddress,
+        uint256 amount
+    ) external returns (bool success);
+
+    /// @dev Defines a method for performing a redelegation
+    /// of coins from a delegator and source validator to a destination validator.
+    /// @param delegatorAddress The address of the delegator
+    /// @param validatorSrcAddress The validator from which the redelegation is initiated
+    /// @param validatorDstAddress The validator to which the redelegation is destined
+    /// @param amount The amount of the bond denomination to be redelegated to the validator
+    /// This amount should use the bond denomination precision stored in the bank metadata.
+    /// @return success Whether or not the redelegate was successful
+    function redelegate(
+        address delegatorAddress,
+        string memory validatorSrcAddress,
+        string memory validatorDstAddress,
+        uint256 amount
+    ) external returns (bool success);
+}
